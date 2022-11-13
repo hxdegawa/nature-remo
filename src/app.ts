d8d4a433082155cf232eccc29bd3f2109a1047c7
Hiro Degawa <hxdegawa@gmail.com>
2022-11-13 14:14:26 +0900

    feat: nature remo controller

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,18 @@
-const start = () => {
-  console.log('Do something!')
+import { Cloud as NatureRemo } from 'nature-remo'
+
+const start = async () => {
+  const NatureRemoClient = new NatureRemo(
+    process.env.NATURE_REMO_TOKEN as string
+  )
+
+  const aircon = await NatureRemoClient.listAircon()
+  const airconId = aircon[0]?.id
+
+  NatureRemoClient.updateAirconSettings(airconId, {
+    operation_mode: 'cool',
+    temperature: '16',
+    button: 'power-off',
+  })
 }
 
 start()
